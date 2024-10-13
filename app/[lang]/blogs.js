0b7711087ab69f71464dfd1e80f0053cc2730e10
Rishi Raj Jain <jain71000@gmail.com>
2024-10-13 21:09:07 +0000

    working en blogs

--- app/[lang]/blogs.js
+++ app/[lang]/blogs.js
@@ -1,5 +1,6 @@
 import { globSync } from 'fast-glob'
 import { readFileSync } from 'fs'
+import { join } from 'path'
 import rehypeSanitize from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
@@ -7,18 +8,15 @@ import remarkRehype from 'remark-rehype'
 import 'server-only'
 import { unified } from 'unified'
 
+const blogDir = join(process.cwd(), 'app', 'blogs')
+
 export const getDictionary = async (locale) => {
     const files = globSync('**/*.md', {
-        cwd: `./app/blogs/${locale}`,
+        cwd: join(blogDir, locale),
     })
     const htmlFiles = await Promise.all(files.map(async (file) => {
-        const content = readFileSync(`./app/blogs/${locale}/${file}`)
-        const result = await unified()
-            .use(remarkParse)
-            .use(remarkRehype)
-            .use(rehypeSanitize)
-            .use(rehypeStringify)
-            .process(content)
+        const content = readFileSync(join(blogDir, locale, file))
+        const result = await unified().use(remarkParse).use(remarkRehype).use(rehypeSanitize).use(rehypeStringify).process(content)
         return String(result)
     }))
     return htmlFiles
